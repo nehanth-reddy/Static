11eef2d031b34a76ccca7dc2286b647453457a33
usdeals <you@example.com>
2024-02-15 12:21:11 -0500

    new files

---
+++ Staticv.java
@@ -0,0 +1,34 @@
+
+
+class Mobile
+{
+    String brand;
+    int price;
+    static String name;
+
+    public void show()
+    {
+        System.out.println(brand + " : " + price + " : " + name);
+    }
+}
+
+public class Staticv
+{
+    public static void main(String a[])
+    {
+        Mobile obj1= new Mobile();
+        obj1.brand = "Apple";
+        obj1.price = 1500;
+        Mobile.name = "Smartphone";
+
+        Mobile obj2= new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 1700;
+        Mobile.name = "Smartphone";
+
+        Mobile.name = "phone";
+
+        obj1.show();
+        obj2.show();
+    }
+}
