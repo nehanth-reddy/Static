9622f1546f7dde335262d4a6d8e1e3b9c8439c05
usdeals <you@example.com>
2024-02-16 09:48:13 -0500

    new file with three classes

--- Aliens.java
+++ Aliens.java
@@ -3,12 +3,19 @@ class Human
 {
     private int age;
     private String name;
+  
+    public Human()
+    {
+        System.out.println("in constructor");
+    }
+  
+  
+  
     public int getAge() {
         return age;
     }
-    public void setAge(int a, Human obj) {
-        Human obj1 = obj;
-        obj1.age = a;
+    public void setAge(int age) {
+        this.age = age;
     }
     public String getName() {
         return name;
@@ -24,8 +31,10 @@ public class Aliens
     public static void main(String[] args) 
     {
         Human obj = new Human();
-        obj.setAge(13,obj);
-        obj.setName("Nehanth");
+        Human obj1 = new Human();
+
+        // obj.setAge(13);
+        // obj.setName("Nehanth");
 
         System.out.println(obj.getName() + " : " + obj.getAge());
     }
---
+++ Superv.java
@@ -0,0 +1,42 @@
+
+
+class A
+
+{
+    public A()
+    {
+        super();
+        System.out.println("in A");
+    }
+    public A(int n)
+    {
+        super();
+        System.out.println("in A int");
+    }
+}
+
+class B extends A
+{
+    public B()
+    {
+        super();
+        System.out.println("in B");
+    }
+
+    public B(int n)  
+    {
+        this();
+        System.out.println("in B int");
+    }
+
+}
+
+
+
+public class Superv 
+{
+    public static void main(String a[])
+    {
+        B obj = new B(5);
+    }
+}
