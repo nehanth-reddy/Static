88f113ff9dd2574f32881592d4784788bf8ba388
usdeals <you@example.com>
2024-02-19 15:00:16 -0500

    new files

--- inheritance.java
+++ inheritance.java
@@ -2,12 +2,13 @@ public class inheritance
 {
     public static void main(String[] args)
     {
-        AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
         int r1 = obj.add(4, 5);
         int r2 = obj.sub(7, 3);
         int r3 = obj.multi(4, 5);
         int r4 = obj.div(7, 3);
+        double r5 = obj.power(4,2);
 
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
 }
